3cda94231e54afeeaa7c5e9ac7deab62346fcd84
michaelyao-eb <yaoxiaoxia@easternbay.cn>
2020-10-14 21:24:12 +0800

    done the basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Test()));
+  runApp(MaterialApp(home: NinjaCard()));
 }
 
-class Test extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("I am the app bar?why"),
+        title: Text("Ninja ID Card"),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
+      backgroundColor: Colors.grey[900],
       body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 100),
-          child: Text("Label")),
-      floatingActionButton: FloatingActionButton(
-        child: Text("Click"),
-        onPressed: () {
-          print("Hi");
-        },
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Name",
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Mike",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "Ninja Level",
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "8",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "michaelyaonz@gmail.com",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
