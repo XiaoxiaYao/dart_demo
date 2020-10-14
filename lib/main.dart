1a505b5ad143aef1b83fdb2c7cd17bb83a370dc6
michaelyao-eb <yaoxiaoxia@easternbay.cn>
2020-10-14 21:45:43 +0800

    is this copy cat?

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ void main() {
   runApp(MaterialApp(home: NinjaCard()));
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +57,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-              "8",
+              '$ninjaLevel',
               style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2,
@@ -73,6 +80,10 @@ class NinjaCard extends StatelessWidget {
             )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => ninjaLevel += 1),
+        child: Icon(Icons.add),
       ),
     );
   }
