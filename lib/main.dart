fd0dcc3a8ef97f5a74b49d2945e38aebb986a9d7
michaelyao-eb <yaoxiaoxia@easternbay.cn>
2020-10-19 09:33:50 +0800

    something else

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/pages/chooseLocation.dart';
-import 'package:my_app/pages/home.dart';
-import 'package:my_app/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation()
-    },
   ));
 }
 
