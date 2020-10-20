f835e6fbaaade95092f34c0e2510255c0d6e0b81
XiaoxiaYao <michaelyaonz@gmail.com>
2020-10-20 10:12:44 +0800

    convert into our own model

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,18 @@
 import "package:firebase_auth/firebase_auth.dart";
+import 'package:my_app/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _convertFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _convertFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
