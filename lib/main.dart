06c82b758a14523c2da96cce2128675831b71495
XiaoxiaYao <michaelyaonz@gmail.com>
2020-10-20 09:47:26 +0800

    basic App structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/wrapper.dart';
 
 void main() {
-  runApp(MaterialApp(
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Wrapper(),
+    );
+  }
 }
 
 
