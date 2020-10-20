60a67791ae68bd0d64fdeecfe50baabec0b7795d
XiaoxiaYao <michaelyaonz@gmail.com>
2020-10-20 15:44:30 +0800

    loading

--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:my_app/services/auth.dart';
+import 'package:my_app/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -16,74 +17,83 @@ class _SignInState extends State<SignIn> {
   String email = "";
   String password = "";
   String error = "";
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0,
-        title: Text("Sign In"),
-        actions: [
-          FlatButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text("Register"))
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  onChanged: (value) => {setState(() => email = value)},
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  onChanged: (value) => {setState(() => password = value)},
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  color: Colors.pink[400],
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() {
-                          error = "Sign In failed";
-                        });
-                      }
-                    }
-                  },
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 14),
-                )
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0,
+              title: Text("Sign In"),
+              actions: [
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text("Register"))
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        onChanged: (value) => {setState(() => email = value)},
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        onChanged: (value) =>
+                            {setState(() => password = value)},
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        color: Colors.pink[400],
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() {
+                              loading = true;
+                            });
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+
+                            if (result == null) {
+                              setState(() {
+                                error = "Sign In failed";
+                                loading = false;
+                              });
+                            }
+                          }
+                        },
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14),
+                      )
+                    ],
+                  ),
+                )),
+          );
   }
 }
