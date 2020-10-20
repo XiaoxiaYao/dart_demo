2afbcbc211fee96342b7f2839734d0b7f4632b26
XiaoxiaYao <michaelyaonz@gmail.com>
2020-10-20 16:22:22 +0800

    new brew member

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,6 @@
 import "package:firebase_auth/firebase_auth.dart";
 import 'package:my_app/models/user.dart';
+import 'package:my_app/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -36,6 +37,9 @@ class AuthService {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
+
+      await DatabaseService(uid: user.uid)
+          .updateUserData("0", "new crew member", 100);
       return _convertFirebaseUser(user);
     } catch (e) {
       print(e.toString());
---
+++ lib/services/database.dart
@@ -0,0 +1,16 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class DatabaseService {
+  final CollectionReference brewCollection =
+      Firestore.instance.collection("brews");
+
+  final String uid;
+
+  DatabaseService({this.uid});
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection
+        .document(uid)
+        .setData({'sugars': sugars, 'name': name, 'strength': strength});
+  }
+}
