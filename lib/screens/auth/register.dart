c41468845951261ae945b766809c5f0450ccc2b1
XiaoxiaYao <michaelyaonz@gmail.com>
2020-10-20 11:33:12 +0800

    pass down function as props

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -2,6 +2,10 @@ import "package:flutter/material.dart";
 import 'package:my_app/services/auth.dart';
 
 class Register extends StatefulWidget {
+  final Function toggleView;
+
+  Register({this.toggleView});
+
   @override
   _RegisterState createState() => _RegisterState();
 }
@@ -20,6 +24,14 @@ class _RegisterState extends State<Register> {
         backgroundColor: Colors.brown[400],
         elevation: 0,
         title: Text("Register"),
+        actions: [
+          FlatButton.icon(
+              onPressed: () {
+                widget.toggleView();
+              },
+              icon: Icon(Icons.person),
+              label: Text("Sign in"))
+        ],
       ),
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -2,6 +2,9 @@ import "package:flutter/material.dart";
 import 'package:my_app/services/auth.dart';
 
 class SignIn extends StatefulWidget {
+  final Function toggleView;
+  SignIn({this.toggleView});
+
   @override
   _SignInState createState() => _SignInState();
 }
@@ -20,6 +23,14 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.brown[400],
         elevation: 0,
         title: Text("Sign In"),
+        actions: [
+          FlatButton.icon(
+              onPressed: () {
+                widget.toggleView();
+              },
+              icon: Icon(Icons.person),
+              label: Text("Register"))
+        ],
       ),
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
